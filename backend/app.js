6186f6a9056b51aa91c737272e3e60f81822bcb3
Ashutosh-Panda2004 <ashutosh2004.panda@gmail.com>
2024-10-23 15:46:17 +0530

    Deployed backend putting backend url for frontend connection

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', // Frontend URL (Adjust if different)
+  origin: 'https://project-1-v-2-frontend-ashutoshpanda.onrender.com', // Frontend URL (Adjust if different)
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   credentials: true,
 }));
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance with the base URL proxied by Vite
 const api = axios.create({
-  baseURL: '/api', // Vite will proxy this to http://localhost:5000/api
+  baseURL: 'https://project-1-v-2-ashutoshpanda.onrender.com', // Vite will proxy this to http://localhost:5000/api
 });
 
 export default api;
